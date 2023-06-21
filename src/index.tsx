719f93c849ac436ad3d036ea7fcb15cc0296520b
johnweng001 <zhiyong.weng2@ontario.ca>
2023-06-21 15:32:01 -0400

    init changes for using geoview

---
+++ src/index.tsx
@@ -0,0 +1,15 @@
+import ReactDOM from "react-dom";
+
+import { App } from "./app";
+import { Provider } from "react-redux";
+import store from "./store";
+import { createContext } from "react";
+import "./assets/css/style.scss";
+
+export const AppContext = createContext({ store });
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
